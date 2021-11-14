dcd061e4234bb780d9a5a3c7837b2a2aad80cefd
Sohel Shaikh <sohelshaikh97@gmail.com>
2021-11-14 11:56:22 +0530

    Add initial player ui

--- app/src/main/java/com/thesohelshaikh/songify/ui/MainActivity.kt
+++ app/src/main/java/com/thesohelshaikh/songify/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.thesohelshaikh.songify
+package com.thesohelshaikh.songify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.thesohelshaikh.songify.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.item_song_player)
     }
 }
