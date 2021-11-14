a815a9fb9e370f3d3707314ca502a93b5954c2dc
Sohel Shaikh <sohelshaikh97@gmail.com>
2021-11-14 12:04:53 +0530

    Add ViewBinding

--- app/src/main/java/com/thesohelshaikh/songify/ui/MainActivity.kt
+++ app/src/main/java/com/thesohelshaikh/songify/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.thesohelshaikh.songify.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.thesohelshaikh.songify.R
+import com.thesohelshaikh.songify.databinding.ItemSongPlayerBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ItemSongPlayerBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.item_song_player)
+        binding = ItemSongPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
